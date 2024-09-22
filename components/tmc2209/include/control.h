aa8e47bf549ed1116460c9082173a22e1ddceb94
Yuriy <angkira@yandex.ru>
2024-09-22 14:33:11 +0200

    Reconfiguration finished

--- components/tmc2209/include/control.h
+++ components/tmc2209/include/control.h
@@ -7,6 +7,8 @@ void enable_driver(TMC2209_Driver *driver);
 void disable_driver(TMC2209_Driver *driver);
 void moveAtVelocity(TMC2209_Driver *driver, int32_t velocity);
 
+void set_target_velocity(TMC2209_Driver *driver, int32_t velocity);
+
 void rotate_motor(TMC2209_Driver *driver, int32_t steps, uint32_t speed);
 
 #endif // TMC2209_CONTROL_H
--- components/tmc2209/include/setup.h
+++ components/tmc2209/include/setup.h
@@ -10,7 +10,6 @@ esp_err_t get_driver_status(TMC2209_Driver *driver);
 
 void set_microsteps(TMC2209_Driver *driver, uint16_t microsteps);
 void set_RMS_Current(TMC2209_Driver *driver, uint16_t mA);
-void set_target_velocity(TMC2209_Driver *driver, int32_t velocity);
 
 uint8_t current_setting_to_percent(uint8_t current_setting);
 uint8_t hold_delay_setting_to_percent(uint8_t hold_delay_setting);
