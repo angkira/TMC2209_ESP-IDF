c37d918344eb58aa9a1e74dd8c6900109faba2b0
Yuriy <angkira@yandex.ru>
2024-09-22 18:48:03 +0200

    Chopmode signature

--- components/tmc2209/include/setup.h
+++ components/tmc2209/include/setup.h
@@ -14,4 +14,14 @@ void set_RMS_Current(TMC2209_Driver *driver, uint16_t mA);
 uint8_t current_setting_to_percent(uint8_t current_setting);
 uint8_t hold_delay_setting_to_percent(uint8_t hold_delay_setting);
 
+typedef enum
+{
+  CHOPPER_MODE_STEALTHCHOP,
+  CHOPPER_MODE_SPREADCYCLE
+} TMC2209_ChopperMode;
+
+void setChopperMode(TMC2209_Driver *driver, TMC2209_ChopperMode mode);
+void writeStealthchopConfig(TMC2209_Driver *driver, const TMC2209_StealthchopConfig *config);
+void writeSpreadCycleConfig(TMC2209_Driver *driver, const TMC2209_SpreadCycleConfig *config);
+
 #endif //  TMC2209_SETUP_H
